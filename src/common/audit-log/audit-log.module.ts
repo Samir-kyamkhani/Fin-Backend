d450503f1e04cb3fed8cb66c4d511607c49cac73
azzunique <azunisoftware18@gmail.com>
2025-12-04 11:33:32 +0530

    auditlog: module add prisma service

--- src/common/audit-log/audit-log.module.ts
+++ src/common/audit-log/audit-log.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuditLogService } from './service/audit-log.service';
+import { PrismaService } from '../../database/database.connection';
 
 @Module({
   imports: [],
-  providers: [AuditLogService],
+  providers: [AuditLogService, PrismaService],
   exports: [AuditLogService],
 })
 export class AuditLogModule {}
